3b4a7ec3872101ae7294e89a7cb9ae8432a65d44
Julianne Ragland <jbragland@gmail.com>
2018-11-01 12:52:45 -0500

    Account for secondary page that has landing template

--- custom-landing.php
+++ custom-landing.php
@@ -8,15 +8,24 @@ $queried_object = get_queried_object();
 $context = Timber::get_context();
 $post = new TimberPost();
 
+if ( $post->post_parent ) {
+	$ancestors = get_post_ancestors($post->ID);
+	$root = count( $ancestors ) - 1;
+	$parent = $ancestors[$root];
+}
+else {
+    $parent = $post->ID;
+}
+
 $this_page_args = array(
     'post_type'         => 'page',
-    'page_id'           => $post->id,
+    'page_id'           => $parent,
 );
 $this_page = Timber::get_posts( $this_page_args );
 
 $other_page_args = array(
     'post_type'         => 'page',
-    'post_parent'       => $post->id,
+    'post_parent'       => $parent,
     'posts_per_page'    => -1,
     'order'             => 'ASC',
     'orderby'           => 'menu_order'
