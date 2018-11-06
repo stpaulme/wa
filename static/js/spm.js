2c24aacd94929652b75cdeed04949f521f183670
Julianne Ragland <jbragland@gmail.com>
2018-11-06 10:37:52 -0600

    Show emergency exit on scroll

---
+++ static/js/spm.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function() {
+    
+    function getAway() {
+      // Get away right now
+      window.open("http://weather.com", "_newtab");
+      // Replace current site with another benign site
+      window.location.replace('http://google.com');
+    }
+
+    jQuery(function() {
+
+      jQuery(".exit").on("click", function(e) {
+        getAway();
+      });
+
+      jQuery(document).keyup(function(e) {
+        if (e.keyCode == 27) { // escape key
+          getAway();
+        }
+      });
+
+    });
+
+    var offset = 40;
+
+    jQuery(window).scroll(function() {
+
+    if (jQuery(this).scrollTop() > offset) {
+
+    jQuery('.exit').fadeIn();
+
+    } else {
+
+    jQuery('.exit').fadeOut();
+
+    }
+
+    });
+    
+});
