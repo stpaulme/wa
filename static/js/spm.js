c89168c91338f020172ef1208c39dbe72d32d6c5
Julianne Ragland <jbragland@gmail.com>
2018-11-06 10:44:54 -0600

    Adjust offset

--- static/js/spm.js
+++ static/js/spm.js
@@ -1,40 +1,36 @@
 jQuery(document).ready(function() {
     
     function getAway() {
-      // Get away right now
-      window.open("http://weather.com", "_newtab");
-      // Replace current site with another benign site
-      window.location.replace('http://google.com');
+        // Get away right now
+        window.open("http://weather.com", "_newtab");
+        // Replace current site with another benign site
+        window.location.replace('http://google.com');
     }
 
     jQuery(function() {
-
-      jQuery(".exit").on("click", function(e) {
-        getAway();
-      });
-
-      jQuery(document).keyup(function(e) {
-        if (e.keyCode == 27) { // escape key
-          getAway();
-        }
-      });
-
-    });
-
-    var offset = 40;
-
-    jQuery(window).scroll(function() {
-
-    if (jQuery(this).scrollTop() > offset) {
-
-    jQuery('.exit').fadeIn();
-
-    } else {
-
-    jQuery('.exit').fadeOut();
-
-    }
-
-    });
+        
+        jQuery(".exit").on("click", function(e) {
+            getAway();
+        });
+        
+        jQuery(document).keyup(function(e) {
+            if (e.keyCode == 27) { // escape key
+                getAway();
+            }
+        });
     
+    });
+
+    var offset = 80;
+    
+    jQuery(window).scroll(function() {
+        
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.exit').fadeIn();
+        } else {
+            jQuery('.exit').fadeOut();
+        }
+    
+    });
+
 });
